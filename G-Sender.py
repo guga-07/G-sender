1ee62a12277fb79934c865849991cfe51a24bad7
Lord Unknown <49972382+guga-07@users.noreply.github.com>
2021-05-29 14:10:17 +0400

    Add files via upload

---
+++ G-Sender.py
@@ -0,0 +1,34 @@
+from colorama import *
+from colorama import init
+try:
+    init()
+    print(Fore.GREEN + "Codded By Lord-Unknown")
+    print(Fore.GREEN + """ 
+ ██████╗       ███████╗███████╗███╗   ██╗██████╗ ███████╗██████╗ 
+██╔════╝       ██╔════╝██╔════╝████╗  ██║██╔══██╗██╔════╝██╔══██╗
+██║  ███╗█████╗███████╗█████╗  ██╔██╗ ██║██║  ██║█████╗  ██████╔╝
+██║   ██║╚════╝╚════██║██╔══╝  ██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗
+╚██████╔╝      ███████║███████╗██║ ╚████║██████╔╝███████╗██║  ██║
+ ╚═════╝       ╚══════╝╚══════╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+                                                                 
+""")
+    print("Allow Program Permissions")
+    print ("1) Go to the this site https://myaccount.google.com/lesssecureapps")
+    print("2) Give Permission secure app access ")
+    import smtplib
+    from getpass import getpass
+    username = input("Your Gmail: ")
+    password = getpass()
+    c = input("Send to: ")
+    msg = input("Message: ")
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(username,c, msg)
+    server.quit()
+    print("Sent Succsesfully")
+except:
+    print("Not Sent")
+
+
