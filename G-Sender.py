094cf0ea7d266deb34e68cacb704ef269af91856
Lord Unknown <49972382+guga-07@users.noreply.github.com>
2021-06-16 00:10:59 +0400

    Update G-Sender.py

--- G-Sender.py
+++ G-Sender.py
@@ -2,10 +2,11 @@ import smtplib
 from colorama import *
 from colorama import init
 from getpass import getpass
-try:
-    init()
-    print(Fore.GREEN + "Codded By Lord-Unknown")
-    print(Fore.GREEN + """ 
+def program():
+    try:
+        init()
+        print(Fore.GREEN + "Codded By Lord-Unknown")
+        print(Fore.GREEN + """ 
  ██████╗       ███████╗███████╗███╗   ██╗██████╗ ███████╗██████╗ 
 ██╔════╝       ██╔════╝██╔════╝████╗  ██║██╔══██╗██╔════╝██╔══██╗
 ██║  ███╗█████╗███████╗█████╗  ██╔██╗ ██║██║  ██║█████╗  ██████╔╝
@@ -14,22 +15,36 @@ try:
  ╚═════╝       ╚══════╝╚══════╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
                                                                  
 """)
-    print("Allow Program Permissions")
-    print ("1) Go to the this site https://myaccount.google.com/lesssecureapps")
-    print("2) Give Permission secure app access ")
+        print("Allow Program Permissions")
+        print ("1) Go to the this site https://myaccount.google.com/lesssecureapps")
+        print("2) Give Permission secure app access ")
    
-    username = input("Your Gmail: ")
-    password = getpass()
-    c = input("Send to: ")
-    msg = input("Message: ")
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login(username, password)
-    server.sendmail(username,c, msg)
-    server.quit()
-    print("Sent Succsesfully")
-except:
-    print("Not Sent")
+        username = input("Your Gmail: ")
+        password = getpass()
+        c = input("Send to: ")
+        msg = input("Message: ")
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(username,c, msg)
+        server.quit()
+        print("Sent Succsesfully")
+    except:
+        print("Not Sent")
+
+program()
+
+def reboot():
+    restart = input("restart program? type yes/no: ")
+
+    if restart == "yes":
+        program()
+    elif restart == "no":
+        exit()
+    else:
+        reboot()
+reboot()
+
 
 
