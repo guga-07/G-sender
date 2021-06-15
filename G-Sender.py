4e443fe4010411880fb6beb16e3eaa58bda682d7
Lord Unknown <49972382+guga-07@users.noreply.github.com>
2021-06-16 00:00:02 +0400

    Update G-Sender.py

--- G-Sender.py
+++ G-Sender.py
@@ -1,5 +1,7 @@
+import smtplib
 from colorama import *
 from colorama import init
+from getpass import getpass
 try:
     init()
     print(Fore.GREEN + "Codded By Lord-Unknown")
@@ -15,8 +17,7 @@ try:
     print("Allow Program Permissions")
     print ("1) Go to the this site https://myaccount.google.com/lesssecureapps")
     print("2) Give Permission secure app access ")
-    import smtplib
-    from getpass import getpass
+   
     username = input("Your Gmail: ")
     password = getpass()
     c = input("Send to: ")
