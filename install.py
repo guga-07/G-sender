3468acff3fc3e2d1a45b7250f4c6a897c33ed653
Lord Unknown <49972382+guga-07@users.noreply.github.com>
2021-06-16 22:58:54 +0400

    Update install.py

--- install.py
+++ install.py
@@ -1,9 +1,9 @@
 import os
 choose = input("choose os: 1)debian/ubuntu, 2)arch: ")
 if choose == 1:
-  os.system("sudo apt install python3 && sudo apt install python3-pip")
+  os.system("sudo apt install python3-pip")
   os.system("pip3 install colorama")
 if choose == 2"
-os.system("sudo pacman -Sy python && sudo pacman -Sy  pip")
+os.system("sudo pacman -Sy  pip")
 os.system("pip install colorama")
 print("now you can run program :) ")
